fdb14a1376cdc84252f3a709f089f26edcbb69c0
Pranamika Kalita <145934619+pranamika1234@users.noreply.github.com>
2025-12-25 20:27:04 +0530

    Fix Vite config for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+	base: '/handecor/', // This must match your repo name!
 	plugins: [react()],
-	base: '/handecor/', // Set base for GitHub Pages
-	server: { port: 5173 }
-})
+});
