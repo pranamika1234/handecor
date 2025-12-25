d1d0a10144bba73c58736b0e04287548023c39ce
Pranamika Kalita <145934619+pranamika1234@users.noreply.github.com>
2025-12-25 20:48:38 +0530

    Switch to HashRouter for GitHub Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Home from './pages/Home'
 import Shop from './pages/Shop'
@@ -12,16 +12,16 @@ import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
 <React.StrictMode>
-<BrowserRouter>
-<Routes>
-<Route path="/" element={<App />}>
-<Route index element={<Home />} />
-<Route path="shop" element={<Shop />} />
-<Route path="about" element={<About />} />
-<Route path="contact" element={<Contact />} />
-<Route path="product/:id" element={<Product />} />
-</Route>
-</Routes>
-</BrowserRouter>
+<HashRouter>
+	<Routes>
+		<Route path="/" element={<App />}>
+			<Route index element={<Home />} />
+			<Route path="shop" element={<Shop />} />
+			<Route path="about" element={<About />} />
+			<Route path="contact" element={<Contact />} />
+			<Route path="product/:id" element={<Product />} />
+		</Route>
+	</Routes>
+</HashRouter>
 </React.StrictMode>
 )
